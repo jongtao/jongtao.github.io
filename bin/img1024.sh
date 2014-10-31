7f3aacb8e3d904fd121a724bfc91fd60c7a26f89
J. Tao <jon.g.tao@gmail.com>
2014-10-31 01:00:48 -0700

    octave and latex blog

--- bin/img1024.sh
+++ bin/img1024.sh
@@ -2,6 +2,7 @@
 # Scales images so longest side is 1024 pixels
 width=1024
 height=1024
+format=png
 
 mkdir -p img
 echo $@
@@ -9,7 +10,7 @@ echo $@
 for file in $@
 do
 	in=$(basename "$file")
-	out=`echo $in | sed 's/\.+$/\.png/'`
+	out=`echo $in | sed 's/\.+$/\.$format/'`
 	avconv -i $file -q:v 1 -vf\
 	"scale=iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih)"\
 	 img/$out
