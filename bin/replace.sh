31197b0e0f37b2f78ae29ba8e441ba962a74a49b
jongtao <jon.g.tao@gmail.com>
2014-12-21 21:54:48 -0800

    cleaned up bin

--- bin/replace.sh
+++
@@ -1,8 +0,0 @@
-# used to fix id to class
-files=`find -name "index.html"`
-
-for file in $files
-do
-	sed -i.bak -e 's/id="page"/class="page"/g' $file && rm $file.bak
-	sed -i.bak -e 's/id="printable"/class="printable"/g' $file && rm $file.bak
-done
