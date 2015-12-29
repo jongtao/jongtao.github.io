9408bcb1977bd21e57d0d47385295ba13be5e8e7
J. Tao <jon.g.tao@gmail.com>
2015-12-28 22:51:55 -0800

    new post sc 70

--- bin/image_crunch.sh
+++ bin/image_crunch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Scales images to specified maximum dimentions
-width=512
-height=512
+width=900
+height=900
 format=jpeg
+# 2-31. 31 is worst quality
+quality=2
 
 mkdir -p img
 echo $@
@@ -11,7 +13,7 @@ for file in $@
 do
 	in=$(basename "$file")
 	out=`echo $in | sed 's/\.+$/\.$format/'`
-	ffmpeg -i $file -q:v 1 -vf\
+	ffmpeg -i $file -q:v $quality -vf\
 	"scale=iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih)"\
 	 img/$out
 done
