e12d6a4cef11b84473e7c2f454641befd956258b
J. Tao <jon.g.tao@gmail.com>
2015-12-31 23:56:35 -0800

    greatly revised Prox Box post

--- bin/image_crunch.sh
+++ bin/image_crunch.sh
@@ -5,8 +5,9 @@ height=900
 format=jpeg
 # 2-31. 31 is worst quality
 quality=2
+dest=converted_img
 
-mkdir -p img
+mkdir -p $dest
 echo $@
 
 for file in $@
@@ -15,5 +16,5 @@ do
 	out=`echo $in | sed 's/\.+$/\.$format/'`
 	ffmpeg -i $file -q:v $quality -vf\
 	"scale=iw*min($width/iw\,$height/ih):ih*min($width/iw\,$height/ih)"\
-	 img/$out
+	 $dest/$out
 done
