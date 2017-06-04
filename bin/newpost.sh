9f131b1fb9b08700b34741eb57398cdda38e515f
J. Tao <jon.g.tao@gmail.com>
2017-06-04 13:57:46 -0700

    revised newpost script

--- bin/newpost.sh
+++ bin/newpost.sh
@@ -1,19 +1,63 @@
 #!/bin/bash
 
-# run at root directory
+# Creates a new post by creating a new content directory with the DATE as the
+# directory name. The script then appends an entry with the link to the content
+# list page.
+# Run this from the  root directory
 
 if [ ! -f ./404.html ]; then # quickly check if root
-	echo "Not in ROOT directory"
+	echo "Not in ROOT directory: 404.html not found"
+	echo "Usage: `basename $0` [notes/crafts] title" 1>&2
 	exit
 fi
 
+# Check number of arguments
+if [ $# -ne 2 ]; then
+	echo "Invalid number of arguments"
+	echo "Usage: `basename $0` [notes/crafts] title" 1>&2
+	exit 1
+fi
+
+# Check argument validity
+if [ $1 != "notes" ] && [ $1 != "crafts" ]; then
+	echo "Post type $1 not recognized"
+	echo "Usage: `basename $0` [notes/crafts] title" 1>&2
+	exit 1
+fi
+
+
+TYPE=$1
+TITLE=$2
+
 DATE=`date "+%m-%d-%y"`
 DATE2=`date "+%m/%d/%y"`
-ENTRY="\t\t\t<li><a href="pages/$DATE">\n\
-\t\t\t\t$DATE2: </a></li>"
-LINE="'/<ul>/a\\$ENTRY'"
 
 
-mkdir ./blog/pages/$DATE
-cp ./templates/post.html ./blog/pages/$DATE/index.html
-eval sed -i.bak -e $LINE ./blog/index.html
+# Create new post directory and copy template to it
+mkdir ./content/$DATE
+cp ./templates/post.html ./content/$DATE/index.html
+# Insert title into post
+sed -i -e "s/POST_TITLE/$TITLE/g" ./content/$DATE/index.html
+
+
+# Add entry to content list page
+if [ $TYPE == "notes" ]; then
+	ENTRY="<li><a href=\"../content/$DATE\"> $DATE2: $TITLE</a></li>"
+else # TYPE == "crafts"
+	ENTRY="<li><a href=\"../content/$DATE\"> $TITLE</a></li>"
+fi
+
+AFTER="<ul>"
+sed -i -e "/$AFTER/a $ENTRY" ./$TYPE/index.html
+
+
+# Add entry to changelog
+ENTRY="<li><a href=\"../content/$DATE\"> Added $DATE2: $TITLE</a></li>"
+AFTER="<ul>"
+sed -i -e "/$AFTER/a $ENTRY" ./changelog/index.html
+
+
+# Add entry to recent changes
+ENTRY="<li><a href=\"content/$DATE\"> Added $DATE2: $TITLE</a></li>"
+AFTER="<ul>"
+sed -i -e "/$AFTER/a $ENTRY" ./index.html
